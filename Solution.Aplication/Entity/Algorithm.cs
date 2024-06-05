83397251d3e9cabbd4f2464dff75a7269d9d472d
Juan Ignacio Brecevich <60331106+juani48@users.noreply.github.com>
2024-06-05 19:09:11 -0300

    configuring messages and use case for adding cubes

--- Solution.Aplication/Entity/Algorithm.cs
+++ Solution.Aplication/Entity/Algorithm.cs
@@ -5,4 +5,5 @@ public class Algorithm : IEntity
     public int Id { get; set; }
     public int CubeId { get; set; }
     public string Text { get; set; } = "";
+    public string Description { get; set; } = "";
 }
